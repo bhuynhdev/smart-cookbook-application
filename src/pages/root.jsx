2d28a183fa6eea1dcb8072c9add62c38a1b331c7
Bao Huynh <55563202+bhuynhdev@users.noreply.github.com>
2023-10-11 20:26:37 -0400

    Add extra details and headings

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -52,14 +52,14 @@ export async function loader({ request }) {
   - Scroll and read through the recipe detail
   - Click "Start session" button at the end to start a streaming session to the rectangular screen, so that you can start interracting with the recipe without the phone
     `;
-  } else if (url.pathname.includes("session")) {
+  } else if (url.pathname.includes('session')) {
     instructions = `
   - The mobile phone UI is now a log of action took on the wall screen
   - The Wall screen is supposedly controlled through gestures (in real life). Simulation buttons are provided
   - Click Next or Previous to progress through the recipe
   - Click End Session to end the cooking session
   - Clicking on other routes on the mobile app UI (such as Home or Search) will also end the session
-    `
+    `;
   }
 
   return { instructions };
@@ -69,48 +69,67 @@ export default function Root() {
   const { instructions } = useLoaderData();
 
   return (
-    <div className="layout grid grid-cols-1 gap-32 md:grid-cols-[1fr_auto]">
-      <div className="frame flex flex-col px-4 py-6">
-        <header className="mb-4 flex items-center justify-between">
-          <Link to="#">LOGO</Link>
-          <nav>
-            <ul className="flex items-center justify-end gap-3">
-              <li className="list-none">
-                <Link to="">Home</Link>
-              </li>
-              <li className="list-none">
-                <Link to="search">Search</Link>
-              </li>
-              <li className="list-none">
-                <button type="button" className="aspect-square w-10 rounded-[50%] bg-slate-600 p-0 text-center">
-                  BH
-                </button>
-              </li>
-            </ul>
-          </nav>
-        </header>
-        <Outlet />
-      </div>
+    <main className="space-y-2">
+      <h1 className="text-center text-2xl font-bold">Smart Coookbook Interfaces - Bao Huynh</h1>
+      <p className="text-center">Use full screen mode to experience the demo to the fullest.</p>
+      <h2 className="text-xl font-bold">Project description</h2>
+      <p>
+        This app is a demo of the Smart Cookbook Interface designed by Bao Huynh, with the goals of allowing users to
+        search and filter recipes to their liking, and then interract with the recipe hands-free while cooking
+      </p>
+      <p>As a result, the designed interfaces have two main hardware components</p>
+      <ul className="ml-4 list-disc">
+        <li>
+          The Left Screen is the accompanying mobile app (sized to the iPhone aspect ratio) which contains the
+          interractive interface with the recipe database
+        </li>
+        <li>
+          The Right Screen represents a separated (probably wall-mounted) screen which streams the recipe content from
+          the mobile app. It is supposedly controlled through gesture so that users needn&apos;t touch anything while
+          cooking, though simulation buttons are provided
+        </li>
+      </ul>
+      <h2 className="text-xl font-bold">Demo app</h2>
+      <div className="layout grid grid-cols-1 gap-32 md:grid-cols-[1fr_auto]">
+        <div className="frame flex flex-col px-4 py-6">
+          <header className="mb-4 flex items-center justify-between">
+            <Link to="#">LOGO</Link>
+            <nav>
+              <ul className="flex items-center justify-end gap-3">
+                <li className="list-none">
+                  <Link to="">Home</Link>
+                </li>
+                <li className="list-none">
+                  <Link to="search">Search</Link>
+                </li>
+                <li className="list-none">
+                  <button type="button" className="aspect-square w-10 rounded-[50%] bg-slate-600 p-0 text-center">
+                    BH
+                  </button>
+                </li>
+              </ul>
+            </nav>
+          </header>
+          <Outlet />
+        </div>
 
-      <div>
-        <SmartDevice />
-        <div className="instructions mt-6 space-y-2">
-          <p className="font-bold">Instructions</p>
-          <p>The Left Screen is the mobile app UI, which has been sized to model iPhone aspect ratio</p>
-          <p>
-            The Right Screen is a wall-mounted device screen streams the recipe content from the mobile app and is
-            supposedly controlled through gesture so that users needn&apos;t touch anything while cooking
-          </p>
-          <p>
-            The below instructions texts show the actions a user can take, and update as you navigate through the pages: 
-          </p>
-          <ul>
-            {instructions.split('\n').map((text, i) => (
-              <li key={i}>{text.trim()}</li>
-            ))}
-          </ul>
+        <div>
+          <SmartDevice />
+          <div className="instructions mt-6 space-y-2">
+            <p className="font-bold">Instructions</p>
+
+            <p>
+              The below instructions texts show the actions a user can take, and update as you navigate through the
+              pages:
+            </p>
+            <ul>
+              {instructions.split('\n').map((text, i) => (
+                <li key={i}>{text.trim()}</li>
+              ))}
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
